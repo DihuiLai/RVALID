f5154240df9cf80934f11914a78656632baac4eb
Dihui Lai <DLai@dihuis-mbp.dhcp.wustl.edu>
2015-04-19 22:04:02 -0500

    First edition of RVALID

---
+++ R/cross_validation.R
@@ -0,0 +1,27 @@
+
+#' 
+#'@import 
+#'@export
+#'@example
+#'utils::data(anorexia, package = "MASS")
+#'anorex.1 <- glm(Postwt ~ Prewt + Treat + offset(Prewt), family = gaussian, data = anorexia)
+#'kfold_validation(anorex.1, 10)
+kfold_validation <- function(object,...) UseMethod("kfold_validation")
+
+kfold_validation.glm<-function(object, k){
+  nr=eval(call("dim",  anorex.1$call$data), envir=parent.frame())[1]
+  group_index = split(1:nr, cut(1:nr, k))
+  valid_prediction=NULL
+  for (i in seq(1, length(group_index))){
+    train_index=unlist(group_index[-i])
+    #print(train_index)
+    #print(class(train_index))
+    test_index=unlist(group_index[i])    
+    data_train = eval(call("[", object$call$data), envir = parent.frame())[train_index, ]
+    #print(dim(data_train))
+    data_test = eval(call("[", object$call$data), envir = parent.frame())[test_index, ]
+    train_model = eval(call("glm",object$call$formula, data=data_train, family=anorex.1$call$family))
+    valid_prediction = c(valid_prediction, predict(train_model, data_test, type="response"))     
+  }
+  return(valid_prediction)
+}
